028fac34dbf2ff1422ff616c909c3476f76c462f
yonedaaan <79830771+yonedaaan@users.noreply.github.com>
2021-03-27 18:56:03 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>SUKIMAJIKAN</h1>
+
+
+    <form method="post" action="insert.php">
+        <p>すきま時間(分):<br><input type="number" name="time" size="20"></p>
+        <p>コメント:<br><input type="text" name="text" size="20"></p>
+        <p><input type="submit" value="送信"></p>
+        </form>
+       
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,37 @@
+<?php
+//1. POSTデータ取得
+
+//まず前のphpからデーターを受け取る（この受け取ったデータをもとにbindValueと結びつけるため）
+$time = $_POST["time"];
+$text = $_POST["text"];
+
+//2. DB接続します xxxにDB名を入力する
+//ここから作成したDBに接続をしてデータを登録します xxxxに作成したデータベース名を書きます
+// mamppの方は
+// $pdo = new PDO('mysql:dbname=xxx;charset=utf8;host=localhost', 'root', 'root');
+
+try {
+    $pdo = new PDO('mysql:dbname=sukima;charset=utf8;host=localhost', 'root', 'root');
+} catch (PDOException $e) {
+    exit('DbConnectError:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成 //ここにカラム名を入力する
+//xxx_table(テーブル名)はテーブル名を入力します
+$stmt = $pdo->prepare("INSERT INTO suki_table(id, time, text)VALUES(NULL, :a1, :a2");
+$stmt->bindValue(':a1', $time, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a2', $text, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if ($status==false) {
+    //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+    $error = $stmt->errorInfo();
+    exit("QueryError:".$error[2]);
+} else {
+    //５．index.phpへリダイレクト 書くときにLocation: in この:　のあとは半角スペースがいるので注意！！
+    header("Location: index.php");
+    exit;
+}
+?>
